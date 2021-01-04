95918811d37fce7df757a8e5d54cbc772e659bbc
saimohanreddyt <tsaimohanreddy@gmail.com>
2021-01-04 15:48:15 +0530

    Add files via upload

--- service/video-service.go
+++ service/video-service.go
@@ -1,1 +1,26 @@
+package service
 
+import "gitlab.com/saimohanreddyt/golang-gin-poc/entity"
+
+type VideoService interface {
+	Save(entity.Video) entity.Video
+	FinAll() []entity.Video
+}
+
+type videoService struct {
+	videos []entity.Video
+}
+
+type New() VideoService{
+	return &videoService{}
+}
+
+func (service *videoService) Save(video entity.Video) entity.Video {
+	service.videos = append(service.videos, video)
+	return video
+}
+
+func (service *videoService) FinAll() []entity.Video {
+	return services.videos
+
+}
