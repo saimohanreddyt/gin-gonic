4fb75a5359480d635d52459cf4b62d08f456054b
saimohanreddyt <tsaimohanreddy@gmail.com>
2021-01-04 15:47:27 +0530

    Add files via upload

--- controller/video-controller.go
+++ controller/video-controller.go
@@ -1,1 +1,33 @@
+package controller
 
+import (
+	"github.com/gin-gonic/gin"
+	"gitlab.com/saimohanreddyt/golang-gin-poc/entity"
+	)
+
+type VideoController interface {
+	Findall() []entity.Video
+	Save(ctx *gin.Context) entity.Video
+
+}
+
+type controller struct {
+	service service.VideoService
+}
+
+func New(service service.VideoService) VideoController {
+	return controller {
+		service: service,
+	}
+}
+
+func (c *controller) Findall() []entity.Video {
+	return service().FindAll()
+
+}
+func (c *controller) Save(ctx *gin.Context) entity.Video{
+	var video entity.Video
+	ctx.BindJSON(&video)
+	c.service.Save(video)
+	return video
+}
