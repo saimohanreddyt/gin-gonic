f5058dde48d1feb3a453524555227375d1a29cee
saimohanreddyt <tsaimohanreddy@gmail.com>
2021-01-05 18:10:01 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,9 @@
+module gilab.com/saimohanreddyt/gin-gonic
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/tpkeeper/gin-dump v1.0.0
+	gopkg.in/go-playground/validator.v9 v9.31.0
+)
--- server.go
+++ server.go
@@ -1,22 +1,60 @@
 package main
 
 import (
+	"io"
+	"net/http"
+
+	"os"
+
+	"gilab.com/saimohanreddyt/gin-gonic/controller"
+	"gilab.com/saimohanreddyt/gin-gonic/middlewares"
+	"gilab.com/saimohanreddyt/gin-gonic/service"
 	"github.com/gin-gonic/gin"
-	"gitlab.com/saimohanreddyt/golang-gin-poc/service"
 )
+
 var (
-	videoServices service.videoService = server.New()
-	videoController controller.VideoController = controller.New(videoServices)
+	videoService    service.VideoService       = service.New()
+	videoController controller.VideoController = controller.New(videoService)
 )
+
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
 
-	server.GET("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.FindAll())
-	})
+	setupLogOutput()
 
-	server.POST("/videos", func(ctx *gin.Context) {
-		ctx.JSON(200, videoController.Save(ctx))
-	})
-	server.Run(":8080")
+	server := gin.New()
+
+	server.Use(gin.Recovery(), middlewares.Logger())
+
+	server.Static("/css", "./templates/css")
+
+	server.LoadHTMLGlob("templates/*.html")
+
+	apiRoutes := server.Group("/api")
+	{
+		apiRoutes.GET("/videos", func(ctx *gin.Context) {
+			ctx.JSON(200, videoController.FindAll())
+		})
+
+		apiRoutes.POST("/videos", func(ctx *gin.Context) {
+			err := videoController.Save(ctx)
+			if err != nil {
+				ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"message": "Video Input is Valid!!"})
+			}
+
+		})
+	}
+
+	viewRoutes := server.Group("/view")
+	{
+		viewRoutes.GET("/videos", videoController.ShowAll)
+	}
+
+	server.Run(":8090")
 }
